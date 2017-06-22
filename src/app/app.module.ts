a1175f9cd99f089a661b1f3376161d65ca23d044
lsalejandrosb <luisalejandrosb@outlook.com>
2017-06-22 11:08:05 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,23 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { HttpModule } from '@angular/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Geolocation } from '@ionic-native/geolocation';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+apiKey: "AIzaSyCuQbRC3V2RF8WTJ0oG83qlZz89yQARqHQ",
+  authDomain: "geoloc-24b81.firebaseapp.com",
+  databaseURL: "https://geoloc-24b81.firebaseio.com",
+  projectId: "geoloc-24b81",
+  storageBucket: "geoloc-24b81.appspot.com",
+  messagingSenderId: "600758599567"
+};
+
 
 @NgModule({
   declarations: [
@@ -13,8 +27,11 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
+  BrowserModule,
+  HttpModule,
+  IonicModule.forRoot(MyApp),
+  AngularFireModule.initializeApp(firebaseConfig,'demo104'),
+  AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +41,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Geolocation
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component , ViewChild, ElementRef} from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
+import { FirebaseListObservable, AngularFireDatabase  } from 'angularfire2/database';
+import { Geolocation } from '@ionic-native/geolocation';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +13,116 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  tasks: FirebaseListObservable<any>;
+  lat:any = 0.0;
+  lon:any = 0.0;
+
+  @ViewChild('mapContainer') mapContainer: ElementRef;
+  map: any;
+
+
+  constructor(
+    public navCtrl: NavController,
+    public alertCtrl: AlertController,
+    public database: AngularFireDatabase,
+    public geolocation: Geolocation,
+    public http: Http
+  ) {
+        this.geolocation.getCurrentPosition().then((resp) => {
+    // resp.coords.latitude
+    // resp.coords.longitude
+        this.lat = resp.coords.latitude;
+        this.lon = resp.coords.longitude;
+
+    }).catch((error) => {
+    console.log('Error getting location', error);
+    });
+
+    this.tasks = this.database.list('/Registros');
 
   }
 
+
+  ionViewWillEnter() {
+    this.displayGoogleMap();
+    this.getMarkers();
+  }
+
+  displayGoogleMap() {
+      let latLng = new google.maps.LatLng(-25.28443775, -57.75512695);
+
+      let mapOptions = {
+        center: latLng,
+        zoom:6,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+      this.map = new google.maps.Map(this.mapContainer.nativeElement, mapOptions);
+    }
+
+    getMarkers() {
+      this.http.get('https://geoloc-24b81.firebaseio.com/Registros.json')
+        .map(res => res.json())
+        .subscribe(data => {
+        this.addMarkersToMap(data);
+        console.log(data);
+
+      });
+    }
+
+    addMarkersToMap(markers) {
+      for(let marker of markers) {
+        var position = new google.maps.LatLng(marker.lat, marker.long);
+        var myMarker = new google.maps.Marker({position: position, title: marker.title});
+        myMarker.setMap(this.map);
+      }
+    }
+
+
+    createTask(){
+    let newTaskModal = this.alertCtrl.create({
+      title: 'Nuevo Registro',
+      message: "Ingresar Nuevo Registro",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Nombre y Apellido'
+        },
+        {
+          name: 'ci',
+          placeholder: 'Cedula de Identidad'
+        },
+        {
+          name: 'zona',
+          placeholder: 'Zona'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.tasks.push({
+              nombreapellido: data.title,
+              ci: data.ci,
+              zona: data.zona,
+              lat: this.lat,
+              long: this.lon
+            });
+          }
+        }
+      ]
+    });
+    newTaskModal.present( newTaskModal );
+  }
+
+
+
+  removeTask( task ){
+    this.tasks.remove( task.$key );
+  }
 }
